8f8416ebd01ca1f26e98adfd77327a047aba4540
Eldritch Cheese <eldritch.cheese@gmail.com>
2022-04-16 08:52:20 -0500

    Exit on any key, not just enter

--- base.py
+++ base.py
@@ -3,7 +3,10 @@
 from base64 import b64decode
 from functools import reduce
 import shutil
+import termios
+import tty
 import os
+import sys
 
 from PIL import Image
 
@@ -67,17 +70,31 @@ class AlternateScreen:
         print("\0337\033[?47h", end="")
         return self
 
-    def __exit__(self, exc_type, exc_value, traceback):
+    def __exit__(self, *args):
         print("\033[2J\033[?47l\0338", end="")
 
 
 class HideCursor:
     def __enter__(self):
-        print("\033[?25h", end="")
+        print("\033[?25l", end="")
         return self
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        print("\033[?25l", end="")
+    def __exit__(self, *args):
+        print("\033[?25h", end="")
+
+
+class UnbufferStdin:
+    def __init__(self):
+        self.fd = sys.stdin.fileno()
+        self.prev_settings = None
+
+    def __enter__(self):
+        self.prev_settings = termios.tcgetattr(self.fd)
+        tty.setraw(self.fd)
+        return self
+
+    def __exit__(self, *args):
+        termios.tcsetattr(self.fd, termios.TCSADRAIN, self.prev_settings)
 
 
 def ansi_print_sequence(image, size=None, skip_last_pixel=True):
@@ -101,9 +118,9 @@ def ansi_print_sequence(image, size=None, skip_last_pixel=True):
 def main():
     to_print = "".join(ansi_print_sequence(im))
 
-    with AlternateScreen(), HideCursor():
+    with AlternateScreen(), HideCursor(), UnbufferStdin():
         print(to_print, end="")
-        input()
+        sys.stdin.read(1)
 
 
 if __name__ == "__main__":
